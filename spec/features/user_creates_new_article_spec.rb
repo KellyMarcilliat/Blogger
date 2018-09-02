4148f554233659b622d7961c37f55e64a807af39
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-09-01 20:59:07 -0600

    strong params and create action

--- spec/features/user_creates_new_article_spec.rb
+++ spec/features/user_creates_new_article_spec.rb
@@ -7,12 +7,12 @@ describe "user creates a new article" do
         visit articles_path 
         click_link "Create a New Article"
         
+        # save_and_open_page
         expect(current_path).to eq(new_article_path) 
         
         fill_in "article[title]", with: "New Title!" 
         fill_in "article[body]", with: "New Body!"
         click_on "Create Article" 
-        save_and_open_page
         
         expect(page).to have_content("New Title!") 
         expect(page).to have_content("New Body!") 
